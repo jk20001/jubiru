04220a9cd7bcfb6062f24c9ccd7fea3ca091de57
jk20001 <jk20001@mail.ru>
2021-09-02 12:29:20 +0300

    Delete dbo.Table_Tarrifs.sql

--- dbo.Table_Tarrifs.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Tariffs]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Condition] NCHAR(5) NULL, 
-    [ConditionValue] FLOAT NULL, 
-    [Operation] NCHAR(5) NULL, 
-    [Factor] FLOAT NULL, 
-    [Description] TEXT NULL
-)
